3f29f192b60fe31fc1f4333f5600f6c6901cb982
joriwind <j.winderickx@gmail.com>
2017-06-01 16:10:12 +0200

    DBCommand structs

---
+++ hecomm/dbtype.go
@@ -0,0 +1,30 @@
+package hecomm
+
+/*
+ *	The structs to be used to communicate with the fog implementation - DBCommands
+ */
+
+//CIType Type of the iot interface
+type CIType int
+
+//Defines the CIType's possible
+const (
+	CISixlowpan CIType = iota + 1
+	CILorawan
+)
+
+//DBCPlatform the struct used for passing information about the platform
+type DBCPlatform struct {
+	Address string
+	CI      CIType
+	CIArgs  map[string]interface{}
+}
+
+//DBCNode the struct used to pass information about a node
+type DBCNode struct {
+	DevEUI     []byte
+	PlAddress  string
+	PlType     CIType
+	IsProvider bool
+	InfType    int
+}
